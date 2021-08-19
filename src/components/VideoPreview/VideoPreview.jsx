ebc3db0f0fde20e5903d8807e7dc537982b2f939
Ryan <ryan@ryannd.com>
2021-08-19 13:43:08 -0500

    Detailed structure for VideoPreview, VideoThumbnail components

--- src/components/VideoPreview/VideoPreview.jsx
+++ src/components/VideoPreview/VideoPreview.jsx
@@ -1,7 +1,25 @@
 import React from 'react';
+import VideoInfo from '../VideoInfo/VideoInfo';
+import VideoThumbnail from '../VideoThumbnail/VideoThumbnail';
 
 export default VideoPreview = (props) => {
+    const videoId = props.videoId;
+    // Get info and thumbnail from YouTube api?
+
+    const imgurl="";
+    const info="";
+
+    /*
+    layout concept:
+    VideoThumbnail on left (pass in imgurl)
+    VideoInfo on right (pass in info)
+    */
+
+
     return (
-        <h1>Under construction</h1>
+        <div>
+            <VideoThumbnail imgurl={imgurl} />
+            <VideoInfo info={info} />
+        </div>
     )
 }
--- src/components/VideoThumbnail/VideoThumbnail.jsx
+++ src/components/VideoThumbnail/VideoThumbnail.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 
 export default VideoThumbnail = (props) => {
+
+    // just return a formatted image from the thumbnail url
     return (
         <h1>Under construction</h1>
     )
