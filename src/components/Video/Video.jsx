4971541bbd032e448907e8cec1f80b08c5171255
Ryan <ryan@ryannd.com>
2021-08-20 12:42:22 -0500

    Implemented useWaitData on Video component

--- src/components/Video/Video.jsx
+++ src/components/Video/Video.jsx
@@ -1,35 +1,38 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
+import useWaitData from '../../hooks/useWaitData';
+
 import VideoPlayer from '../VideoPlayer/VideoPlayer';
 import VideoInfo from '../VideoInfo/VideoInfo';
 import Comments from '../Comments/Comments';
 
 const Video = (props) => {
     const videoId = props.videoId;
-    let [videoData, setVideoData] = useState({});
+    // let [videoData, setVideoData] = useState({});
     let [videoInfo, setVideoInfo] = useState(false);
 
-    async function getVideo(videoId) {
-        const response = await axios.get(`https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&id=${videoId}&key=AIzaSyCrQ8epCYClv4Shg5vi1y3u4-BC4PGq7Mg`);
-        
-        if (response.data) {
-            setVideoData(response.data.items[0]);
-        }
-    }
+    let {videoData} = useWaitData(`https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&id=${videoId}&key=AIzaSyCrQ8epCYClv4Shg5vi1y3u4-BC4PGq7Mg`);
 
-    useEffect(() => {
-        getVideo(videoId)
-    }, [videoId])
+    // async function getVideo(videoId) {
+    //     const response = await axios.get(`https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&id=${videoId}&key=AIzaSyCrQ8epCYClv4Shg5vi1y3u4-BC4PGq7Mg`);
+        
+    //     if (response.data) {
+    //         setVideoData(response.data.items[0]);
+    //     }
+    // }
+
+    // useEffect(() => {
+    //     getVideo(videoId)
+    // }, [videoId])
     
     useEffect(() => {
-        console.log(videoData)
-        if (videoData.snippet && !videoInfo) {
-            debugger;
+        if (videoData.items && !videoInfo) {
+            let relevantData = videoData.items[0].snippet;
             setVideoInfo({
-                title: videoData.snippet.title,
-                creator: videoData.snippet.channelTitle,
-                description: videoData.snippet.description
+                title: relevantData.title,
+                creator: relevantData.channelTitle,
+                description: relevantData.description
             });
         }
     }, [videoData])
